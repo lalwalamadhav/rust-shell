d993673d8c9c8bfb143054fff4c6e3a422a04b46
Madhav Lalwala <lalwalamadhav@gmail.com>
2022-05-05 19:00:47 +0530

    Working Highlighter

--- src/highlight.rs
+++ src/highlight.rs
@@ -1,50 +1,46 @@
 use colored::Colorize;
-use std::io;
-mod process_command
+use std::io::{self,Write};
+mod process_command;
 
 enum Input {
-    ValidCommand(str),
-    InvalidCommand(str),
-    Arguments(str),
-    Quotes(str),
+    ValidCommand(String),
+    InvalidCommand(String),
+    Arguments(String),
+    Quotes(String),
 }
 
 impl Input {
     fn highlight(&self) {
         match self {
-            Input::ValidCommand(k) => print!("{}",format!("{}", k).expect("Error Highlighting").blue()),
-            Input::InvalidCommand(k) => print!("{}",format!("{}", k).expect("Error Highlighting").red()),
-            Input::Arguments(k) => print!(" {}",format!("{}", k).expect("Error Highlighting").cyan()),
-            Input::Quotes(k) => print!("{}",format!("{}", k).expect("Error Highlighting").yellow()),
+            Input::ValidCommand(k) => print!("{}",format!("{}", k).blue()),
+            Input::InvalidCommand(k) => print!("{}",format!("{}", k).red()),
+            Input::Arguments(k) => print!(" {}",format!("{}", k).cyan()),
+            Input::Quotes(k) => print!("{}",format!("{}", k).yellow()),
         };
         io::stdout().flush().unwrap();
     }
 }
 
-pub fn highlighter(stdin: &String) {
+pub fn highlighter(raw_input: &String, command_list: Vec<String>) {
+    let mut arg: Vec<Input> = vec![];
+    let (command, args) = process_command::comm(&raw_input);
     let input: Input;
-    let arg: Vec<Input> = vec![];
-    for line in stdin.lock().lines() {
-        if line == "\n" {
-            break;
+    if command_list.contains(&command) {
+        input = Input::ValidCommand(String::from(&command));
+    } else {
+        input = Input::InvalidCommand(String::from(&command));
+    }
+    for i in args.iter() {
+        if i.as_bytes()[0] == b'"' {
+            arg.push(Input::Arguments(i.to_string()));
         } else {
-            let (command, args) = process_command::com(&line);
-            if command_list.contains(&command) {
-                let input = Input::ValidCommand(&String::from(&command));
-            } else {
-                let input = Input::ValidCommand(&String::from(&command));
-            }
-        }
-        for i in args.iter() {
-            if &i[0].as_bytes() == b'"' {
-                arg.push(Input::Arguments(i));
-            } else {
-                arg.push(Input::Quotes(i));
-            }
+            arg.push(Input::Quotes(i.to_string()));
         }
     }
     input.highlight();
     for i in arg.iter() {
+        print!(" ");
+	io::stdout().flush().unwrap();
         i.highlight();
     }
 }
