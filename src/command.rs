81c730b77fc0f3edcf1b16fe5387f249096af9d8
Madhav Lalwala <madhav@Alien-Monster>
2023-02-09 06:22:49 +0530

    fixed command.rs

--- src/command.rs
+++ src/command.rs
@@ -6,20 +6,25 @@ struct command {
 impl command {
     fn new(input: &String) -> command {
         let mut dummy_string: String = String::from("");
-        let command_string: String = String::from("");
+        let mut command_string: String = String::from("");
         let mut args: Vec<String> = vec![];
-        for i in 0..input.len() {
-            if input[i] == ' ' {
-                if command_string == String::from("") {
+        let mut done = false;
+        for i in 0..input.len() + 1 {
+            if i == input.len() || input.chars().nth(i).unwrap() == ' ' {
+                if done == false {
                     command_string = dummy_string;
+                    done = true;
                 } else if dummy_string != String::from("") {
-                    args.push(command_string);
+                    args.push(dummy_string);
                 }
                 dummy_string = String::from("");
             } else {
-                dummy_string += input[i];
+                dummy_string.push(input.chars().nth(i).unwrap());
             }
         }
-        command {command: command_string, args: args}
+        command {
+            command: command_string,
+            args: args,
+        }
     }
 }
